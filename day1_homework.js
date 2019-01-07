0c350d5d6fa7d0a0d7f5a9d70658c64e40a6ff2b
Katharina01099 <yes>
2019-01-07 17:19:01 +0000

    1,3,4,5,6,7

---
+++ day1_homework.js
@@ -0,0 +1,98 @@
+//Episode 1
+
+/*
+output will be "The murderer is Miss Scarlet" because const is mutable
+*/
+
+
+// Episode 3
+
+
+/*
+output:
+The First Verdict: The murderer is Mrs. Peacock.
+The Second Verdict: The murderer is Professor Plum.
+--> because let has a lexical scope within the const declareMurderer
+so when declareMurderer is called it uses the let from within the block.
+const secondVerdict is outside the const declareMurderer and so it uses let murderer
+*/
+
+// Episode 4
+
+/*
+1. output: 'Miss Scarlet', 'Professor Plum', Colonel Mustard
+2. output: 'Mrs Peacock'
+because when printing the suspects it uses the let variable from with the variable declareAllSuspects
+and then the other lets declared outside.
+when suspect three is called it uses the first suspectThree variable because it
+cant look inside const declareAllSuspects
+*/
+
+// Episode 5
+
+
+/*
+output: The weapon is the Revolver.
+because: all constants are mutable in this example and
+changeWeapon defines the weapon as Revolver.
+*/
+
+
+// Episode 6
+
+
+/*
+output:
+'The murderer is Mrs White'
+varibale plottwist is called, which changes changeMurderer
+from Mr Green to Mrs White
+*/
+
+// Episode 7
+
+
+/*
+within changeMurderer the variable murderer is not defined so it is
+added to the global scope
+*/
+
+// Episode 8
+
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+/*
+
+*/
