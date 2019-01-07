35a5300801a54432c6116dc45d92287a04f6b2e1
Katharina01099 <yes>
2019-01-07 22:22:40 +0000

    MVP completed

--- day1_homework.js
+++ day1_homework.js
@@ -58,41 +58,15 @@ added to the global scope
 
 // Episode 8
 
-const scenario = {
-  murderer: 'Mrs. Peacock',
-  room: 'Conservatory',
-  weapon: 'Lead Pipe'
-};
-
-const changeScenario = function() {
-  scenario.murderer = 'Mrs. Peacock';
-  scenario.room = 'Dining Room';
-
-  const plotTwist = function(room) {
-    if (scenario.room === room) {
-      scenario.murderer = 'Colonel Mustard';
-    }
-
-    const unexpectedOutcome = function(murderer) {
-      if (scenario.murderer === murderer) {
-        scenario.weapon = 'Candle Stick';
-      }
-    }
-
-    unexpectedOutcome('Colonel Mustard');
-  }
-
-  plotTwist('Dining Room');
-}
-
-const declareWeapon = function() {
-  return `The weapon is ${scenario.weapon}.`
-}
-
-changeScenario();
-const verdict = declareWeapon();
-console.log(verdict);
 
 /*
+The weapon is candle stick because all const are mutable
+*/
+
+// Episode 9
+
+
+/*
+The murderer is Professor Plum because the first let declares the global scope.
 
 */
